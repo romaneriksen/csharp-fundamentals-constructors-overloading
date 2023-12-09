b221034d863310cfe70e9a159b0bafbbfb433725
Nigel <nigel.sibbert@gmail.com>
2023-12-09 22:42:47 +0000

    updated tests

--- Boolean.CSharp.Test/ExtensionTests.cs
+++ Boolean.CSharp.Test/ExtensionTests.cs
@@ -11,20 +11,102 @@ namespace Boolean.CSharp.Test
     [TestFixture]
     public class ExtensionTests
     {
-        //TODO: 1. add, which accepts two floats and returns a float (both floats added together)
+        private Extension _extension;
 
-        //TODO: 2. add, which accepts two doubles and returns a double (both doubles added together)
+        [Test]
+        public void TestQuestion1()
+        {
 
-        //TODO: 3. subtract, which accepts two floats and returns a float (first float minus second float)      
+            float a = 1.0f;
+            float b = 2.0f;
 
-        //TODO: 4. subtract, which accepts a String and a char and returns a string with all instances of the provided char removed        
+            Extension extension = new Extension();
 
-        //TODO: 5. multiply, which accepts two ints and returns an int (first int multiplied by second int)
+            float result = extension.add(a, b);
 
-        //TODO: 6. multiply, which accepts a string and an int, and returns a string containing the provided string as many times as the provided int separated by a comma. E.g. multiply("Hello", 3) -> "Hello,Hello,Hello"
+            Assert.AreEqual(3.0f, result);
 
-        //TODO: 7. multiply, which accepts an array of Strings that each contain a number, and an int. The method should return an array of ints that contain the value of multiplying each String number by the provided int E.g. multiply(["2", "7", "3"], 3) -> [6, 21, 9]
 
+        }
+        [Test]
+        public void TestQuestion2()
+        {
+            double a = 1.0;
+            double b = 2.0;
+
+            Extension extension = new Extension();
+
+            double result = extension.add(a, b);
+
+            Assert.AreEqual(3.0, result);
+        }
+
+        [Test]
+        public void TestQuestion3()
+        {
+            float a = 2.0f;
+            float b = 1.0f;
+
+            Extension extension = new Extension();
+
+            float result = extension.subtract(a, b);
+
+            Assert.AreEqual(1.0f, result);
+        }
+
+        [Test]
+        public void TestQuestion4()
+        {
+            string source = "the quick brown fox jumps over the lazy dog";
+            char z = 'z';
+
+            Extension extension = new Extension();
+
+            string result = extension.subtract(source, z);
+
+            Assert.IsTrue(result == "the quick brown fox jumps over the lay dog");
+        }
+
+        [Test]
+        public void TestQuestion5()
+        {
+            int a = 2;
+            int b = 5;
+
+            Extension extension = new Extension();
+
+            int result = extension.multiply(a, b);
+
+            Assert.AreEqual(10, result);
+        }
+        [Test]
+        public void TestQuestion6()
+        {
+            string source = "Hello";
+            int dupes = 3;
+
+            Extension extension = new Extension();
+
+            string result = extension.multiply(source, 3);
+
+            Assert.IsTrue("Hello,Hello,Hello" == result);
+        }
+        [Test]
+        public void TestQuestion7()
+        {
+            //multiply([], 3) -> [6, 21, 9]
+            string[] source = { "2", "7", "3" };
+            int multiplier = 3;
+
+            Extension extension = new Extension();
+            var result = extension.multiply(source, multiplier);
+
+            int[] answer = { 6, 21, 9 };
+
+            Assert.IsTrue(result.SequenceEqual(answer));
+
+
+        }
 
     }
 }
